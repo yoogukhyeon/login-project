1280740e8d0cdd814405f41ad98190f192fc6442
유국현 <rnrgus5897@gmail.com>
2021-06-13 17:09:45 +0900

    2021 06 / 13 로그인 javascript로 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,3 +2,19 @@
 
 console.log("hello")
 
+
+const id = document.querySelector("#id");
+const pw = document.querySelector("#password");
+const loginBtn = document.querySelector("#login");
+
+
+loginBtn.addEventListener("click", login);
+
+
+function login(){
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    
+}
