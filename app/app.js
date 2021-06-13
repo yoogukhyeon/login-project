59c30ea09ee3e10612d2ff2aa39ff519b00fbfb5
유국현 <rnrgus5897@gmail.com>
2021-06-13 16:56:33 +0900

    2021 06 / 13 app.main js 연결

--- app/app.js
+++ app/app.js
@@ -10,7 +10,8 @@ app.set("view engine","ejs");
 
 
 
-
+app.use(express.static(`${__dirname}/src/public`));
+// app.use(express.static(__dirname + 'src/public'))
 app.use("/" , home); //use = 미들 웨어를 등록해주는 메서드 "/"경로로 들어오면 home 폴더로 보내준다;
 
 module.exports = app;
